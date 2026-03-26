df39c90132b1d3a8329dd46c624f8666a012709e
Caio Sanlay <caiosanlayaraujo@gmail.com>
2026-03-26 16:16:14 -0300

    chore: initial project setup with Spring Boot and PostgreSQL

---
+++ src/main/java/com/caio/flowtrack_api/FlowtrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.caio.flowtrack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowtrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowtrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/flowtrack_api/FlowtrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.flowtrack_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowtrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
